6e43b5f5ea1d9978f1fd3f98cfda0fb66a169d7a
tinkerbells <daniel.kasatkin.work@gmail.com>
2025-04-28 16:32:17 +0300

    feat: add diff layouts for auth and not auth pages

--- src/presentation/core/app.tsx
+++ src/presentation/core/app.tsx
@@ -1,18 +1,18 @@
 import './app.css'
 
+import { Toaster } from 'sonner'
+import { CssBaseline } from '@mui/material'
 import { withErrorBoundary } from 'react-error-boundary'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import { queryClient } from '@/utils/query-client'
-
-import { BrowserRouter } from '.'
-
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import { queryClient } from '@/utils/query-client'
+
+import { BrowserRouter } from './router'
 import { AppProvider } from '../../di/provider'
 import { compose, ErrorHandler, logError } from './react'
 
@@ -23,18 +23,14 @@ const enhance = compose(component =>
   }),
 )
 
-function AppContent() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter />
-      <ReactQueryDevtools position="left" initialIsOpen={false} />
-    </QueryClientProvider>
-  )
-}
-
 const App = enhance(() => (
   <AppProvider>
-    <AppContent />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter />
+      <CssBaseline />
+      <Toaster />
+      <ReactQueryDevtools position="left" initialIsOpen={false} />
+    </QueryClientProvider>
   </AppProvider>
 ))
 
--- src/presentation/core/router.tsx
+++ src/presentation/core/router.tsx
@@ -1,9 +1,6 @@
-import { Toaster } from 'sonner'
 import { observer } from 'mobx-react-lite'
-import CssBaseline from '@mui/material/CssBaseline'
 import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router'
 
-import { Header } from '../common'
 import { root } from './react-router'
 import { decksPageRoute } from '../decks/decks.route'
 import { signInPageRoute } from '../sign-in/sign-in.route'
@@ -12,10 +9,7 @@ import { signUpPageRoute } from '../sign-up/sign-up.route'
 const MainLayout = observer(() => {
   return (
     <>
-      <Header />
       <Outlet />
-      <CssBaseline />
-      <Toaster />
     </>
   )
 })
