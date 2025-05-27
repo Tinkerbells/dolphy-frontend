6dee35e1570a4116eedc9b98a00ea08ec0a51b44
tinkerbells <daniel.kasatkin.work@gmail.com>
2025-05-27 18:00:42 +0300

    feat: add more typization to i18next

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,7 +1,32 @@
 import 'i18next'
 
+import type auth from '../../public/locales/en/auth.json'
+import type decks from '../../public/locales/en/decks.json'
+import type cards from '../../public/locales/en/cards.json'
+import type notes from '../../public/locales/en/notes.json'
+import type common from '../../public/locales/en/common.json'
+import type validation from '../../public/locales/en/validation.json'
+
 declare module 'i18next' {
   interface CustomTypeOptions {
-    // other
+    // Дефолтное пространство имен
+    defaultNS: 'common'
+
+    // Определение ресурсов на основе импортированных JSON
+    resources: {
+      common: typeof common
+      auth: typeof auth
+      validation: typeof validation
+      decks: typeof decks
+      cards: typeof cards
+      notes: typeof notes
+    }
+
+    returnNull: false
+    returnObjects: false
+    keySeparator: '.'
+    nsSeparator: ':'
+    allowObjectInHTMLChildren: false
+    strictKeyChecks: true
   }
 }
